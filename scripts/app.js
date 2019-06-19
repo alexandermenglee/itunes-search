19dc6de0004dbfef89c30c2cae54beb89620e6b1
Alexander Lee <alexanderlee@Alexanders-MacBook-Pro.local>
2019-06-19 16:22:13 -0500

    connected appication to iTunes API and displays song title, artist name and album art

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,35 @@
-alert('connected');
+let endpoint = "";
+let img = document.getElementById('img');
+let userSearch = document.getElementById('user-search');
+let searchBtn = document.getElementById('search');
+let title = document.getElementById('title');
+let artist = document.getElementById('artist');
+let results = document.getElementById('results');
+let HTMLString = `<p id='track-name'>Title: %</p><p id='artis't>Artist: %%</p><img src='%%%'>`;
+
+searchBtn.addEventListener('click', getData);
+
+function getData() {
+  endpoint = "http://itunes.apple.com/search?term=" + userSearch.value.replace(" ", "+");
+  // console.log(userSearch.value);
+  // console.log(endpoint);
+  $.ajax({
+    dataType: "jsonp",
+    method: "GET",
+    url: endpoint
+
+  }).done(function (data) {
+    console.log(data);
+    data.results.forEach((value, i) => {
+      HTMLString = `<div class='add-border'><p id='track-name'>Title: %</p><p id='artis't>Artist: %%</p><img src='%%%'><p><a href='%%%%' target="_blank">Listen To A Sample</a></p></div>`;
+      HTMLString = HTMLString.replace('%', value.trackName);
+      HTMLString = HTMLString.replace('%%', value.artistName);
+      HTMLString = HTMLString.replace('%%%', value.artworkUrl100);
+      HTMLString = HTMLString.replace('%%%%', value.previewUrl);
+      results.innerHTML += HTMLString;
+    });
+  })
+    .fail(function (err) {
+      console.log(err);
+    })
+}
