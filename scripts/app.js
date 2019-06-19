2a9e2387ac0eafc3905a724dcb3b46007097b1ce
Alexander Lee <alexanderlee@Alexanders-MacBook-Pro.local>
2019-06-19 16:50:53 -0500

    added functionality so the DOM clears when the user enters in a new search

--- scripts/app.js
+++ scripts/app.js
@@ -13,23 +13,30 @@ function getData() {
   endpoint = "http://itunes.apple.com/search?term=" + userSearch.value.replace(" ", "+");
   // console.log(userSearch.value);
   // console.log(endpoint);
+  if(results.childElementCount > 0) {
+    $("#results").empty();
+    getData();
+  }
   $.ajax({
     dataType: "jsonp",
     method: "GET",
     url: endpoint
 
-  }).done(function (data) {
-    console.log(data);
-    data.results.forEach((value, i) => {
-      HTMLString = `<div class='add-border'><p id='track-name'>Title: %</p><p id='artis't>Artist: %%</p><img src='%%%'><p><a href='%%%%' target="_blank">Listen To A Sample</a></p></div>`;
-      HTMLString = HTMLString.replace('%', value.trackName);
-      HTMLString = HTMLString.replace('%%', value.artistName);
-      HTMLString = HTMLString.replace('%%%', value.artworkUrl100);
-      HTMLString = HTMLString.replace('%%%%', value.previewUrl);
-      results.innerHTML += HTMLString;
-    });
   })
-    .fail(function (err) {
-      console.log(err);
-    })
+  .done(function (data) {
+     console.log(data);
+     data.results.forEach((value, i) => {
+       HTMLString = `<div class='add-border'><p id='track-name'>Title: %</p><p id='artis't>Artist: %%</p><img src='%%%'><p><a href='%%%%' target="_blank">Listen To A Sample</a></p></div>`;
+       HTMLString = HTMLString.replace('%', value.trackName);
+       HTMLString = HTMLString.replace('%%', value.artistName);
+       HTMLString = HTMLString.replace('%%%', value.artworkUrl100);
+       HTMLString = HTMLString.replace('%%%%', value.previewUrl);
+       results.innerHTML += HTMLString;
+     });
+  })
+  .fail(function (err) {
+     console.log(err);
+   });
 }
+// if results.childcount > 0 - clear dom then run getData
+// else run getData
